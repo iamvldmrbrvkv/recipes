43e3f6f97a3ea3cfc9ba52c35a06777c854169ff
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-22 22:43:10 +0300

    finished lesson

---
+++ src/components/FavoriteButton.jsx
@@ -0,0 +1,8 @@
+export default function FavoriteButton({ children, onClickHandler, icon }) {
+  return (
+    <button className="favorite-button" onClick={onClickHandler}>
+      <img className="heart-icon" alt="" src={icon} />
+      {children}
+    </button>
+  )
+}
---
+++ src/components/Recipe.jsx
@@ -0,0 +1,13 @@
+export default function Recipe({ recipe, children }) {
+  return (
+    <div key={recipe.id} className="recipe" tabIndex={0}>
+      <span className="recipe-container">
+        <h3 className="recipe-name">{recipe.name}</h3>
+        <div className="image-container">
+          <img src={recipe.img} alt="" className="recipe-image" />
+        </div>
+      </span>
+      {children}
+    </div>
+  )
+}
