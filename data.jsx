02c652b6d35dd283db4f90958d9b278336f14de2
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-22 23:39:03 +0300

    fix allRecipesData

--- data.jsx
+++ data.jsx
@@ -11,17 +11,17 @@ import masaladosa from './img/masaladosa.jpg'
 import padthai from './img/padthai.jpg'
 
 const allRecipesData = [
-    { id: 0, name: 'Biscuits', img: biscuits},
-    { id: 1, name: 'Bulgogi', img: bulgogi},
-    { id: 2, name: 'Calamari', img: calamari},
-    { id: 3, name: 'Ceviche', img: ceviche},
-    { id: 4, name: 'Cheeseburger', img: cheeseburger},
-    { id: 5, name: 'Churrasco', img: churrasco},
-    { id: 6, name: 'Dumplings', img: dumplings},
-    { id: 7, name: 'Fish & Chips', img: fishnchips},
-    { id: 8, name: 'Hummus', img: hummus},
-    { id: 9, name: 'Masala Dosa', img: masaladosa},
-    { id: 10, name: 'Pad Thai', img: padthai}
+    { id: 0, name: 'Biscuits', img: biscuits },
+    { id: 1, name: 'Bulgogi', img: bulgogi },
+    { id: 2, name: 'Calamari', img: calamari },
+    { id: 3, name: 'Ceviche', img: ceviche },
+    { id: 4, name: 'Cheeseburger', img: cheeseburger },
+    { id: 5, name: 'Churrasco', img: churrasco },
+    { id: 6, name: 'Dumplings', img: dumplings },
+    { id: 7, name: 'Fish & Chips', img: fishnchips },
+    { id: 8, name: 'Hummus', img: hummus },
+    { id: 9, name: 'Masala Dosa', img: masaladosa },
+    { id: 10, name: 'Pad Thai', img: padthai }
   ];
   
   export default allRecipesData;
