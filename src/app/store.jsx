2dd643cd2b78cc74c2801a4571d8a530c5ddddd9
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-26 20:33:04 +0300

    feat: add Redux Toolkit

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,13 @@
 import { createStore, combineReducers } from 'redux';
-import { favoriteRecipesReducer } from '../features/favoriteRecipes/favoriteRecipesSlice.jsx';
+import { configureStore } from '@reduxjs/toolkit';
+import favoriteRecipesReducer from '../features/favoriteRecipes/favoriteRecipesSlice.jsx';
 import { searchTermReducer } from '../features/searchTerm/searchTermSlice.jsx';
 import { allRecipesReducer } from '../features/allRecipes/allRecipesSlice.jsx';
 
-export const store = createStore(combineReducers({
-  favoriteRecipes: favoriteRecipesReducer,
-  searchTerm: searchTermReducer,
-  allRecipes: allRecipesReducer
-}));
+export const store = configureStore({
+  reducer: {
+    favoriteRecipes: favoriteRecipesReducer,
+    searchTerm: searchTermReducer,
+    allRecipes: allRecipesReducer
+  },
+})
--- src/features/favoriteRecipes/favoriteRecipesSlice.jsx
+++ src/features/favoriteRecipes/favoriteRecipesSlice.jsx
@@ -1,26 +1,17 @@
-const initialState = [];
+import { createSlice } from "@reduxjs/toolkit";
 
-export const favoriteRecipesReducer = (favoriteRecipes = initialState, action) => {
-  switch (action.type) {
-    case 'favoriteRecipes/addRecipe':
-      return [...favoriteRecipes, action.payload]
-    case 'favoriteRecipes/removeRecipe':
-      return favoriteRecipes.filter(recipe => recipe.id !== action.payload.id)
-    default:
-      return favoriteRecipes;
-  }
-}
+export const favoriteRecipesSlice = createSlice({
+  name: 'favoriteRecipes',
+  initialState: [],
+  reducers: {
+    addRecipe: (state, action) => {
+      state.push(action.payload)
+    },
+    removeRecipe: (state, action) => {
+      return state.filter(recipe => recipe.id !== action.payload.id)
+    },
+  },
+})
 
-export function addRecipe(recipe) {
-  return {
-    type: 'favoriteRecipes/addRecipe',
-    payload: recipe
-  }
-}
-
-export function removeRecipe(recipe) {
-  return {
-    type: 'favoriteRecipes/removeRecipe',
-    payload: recipe
-  }
-}
+export const { addRecipe, removeRecipe } = favoriteRecipesSlice.actions
+export default favoriteRecipesSlice.reducer
